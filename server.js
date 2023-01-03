bd95a7f387a3d7679b2bf95e6e4f791b3ae8dc93
KhaledAlabssi <khaled.absi5@gmail.com>
2023-01-03 07:03:13 +0100

    adding mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-import dotenv from 'dotenv'
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db.js";
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {res.send("home page...")})
+//middleware:
+import notFoundMiddleware from "./middleware/notFoundMiddleware.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
-app.listen(5000, ()=>console.log("Running on 5000"))
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("home page...");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => console.log("Server running on " + port));
+  } catch (error) {console.log(error);}
+};
+start();
